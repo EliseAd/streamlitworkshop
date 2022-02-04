1d6f6a06723e34524abbe8ac8a500c3df238e4f5
EliseAd <61750358+EliseAd@users.noreply.github.com>
2022-02-04 15:38:16 +0100

    Update workshop.py

--- workshop.py
+++ workshop.py
@@ -2,9 +2,13 @@ import pandas as pd
 import streamlit as st
 from streamlit import write
 from transformers import pipeline
+from google.cloud import firestore
 import os
 
+
 def app():
+    
+    db = firestore.Client.from_service_account_info(st.secrets["gcp_service_account"])
     st.text("Le meilleur workshop de l'année !!")
     sentence = st.text_input('Fill in the sentence you want to try then press enter:', 'Data science is [MASK].')
     unmasker = pipeline('fill-mask', model='distilbert-base-uncased')
@@ -13,6 +17,12 @@ def app():
         st.write(pd.DataFrame(result))
     else:
         st.warning("The sentence needs to contains [MASK]")
+        
+    if st.button("Store result in the database"):
+        data = {
+            u"table_results": result
+        }
+        db.collection("posts").document(sentence).set(data)
 
 if __name__ == '__main__':
 
